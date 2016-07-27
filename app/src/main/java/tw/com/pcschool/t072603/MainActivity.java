e55a7dda989508eff14326e091a72776a19cacff
shaojung <shaojung@gmail.com>
2016-07-27 09:35:44 +0800

    Add About button

--- app/src/main/java/tw/com/pcschool/t072603/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t072603/MainActivity.java
@@ -32,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add("AA");
+        menu.add("BB");
+        menu.add("CC");
         return true;
     }
 
